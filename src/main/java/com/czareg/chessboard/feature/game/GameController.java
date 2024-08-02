21ec2f52501c21cf15ceccda19c2892da101b8b8
Czareg <czarecoo@gmail.com>
2024-08-02 19:21:55 +0200

    Created game: controller, service, repository and initializer that creates game if none exists

---
+++ src/main/java/com/czareg/chessboard/feature/game/GameController.java
@@ -0,0 +1,37 @@
+package com.czareg.chessboard.feature.game;
+
+import com.czareg.chessboard.feature.game.dto.RandomCommandRequestDTO;
+import com.czareg.chessboard.feature.game.dto.SpecificCommandRequestDTO;
+import com.czareg.chessboard.feature.game.dto.UnitDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class GameController {
+
+    private final GameService gameService;
+
+    @PostMapping("/game")
+    public Game createGame() {
+        return gameService.createGame();
+    }
+
+    @GetMapping("/units")
+    public List<UnitDTO> listUnits() {
+        return gameService.listUnits();
+    }
+
+    @PostMapping("/commands/specific")
+    public void executeSpecificCommand(@RequestBody SpecificCommandRequestDTO specificCommandDTO) {
+        gameService.executeSpecificCommand(specificCommandDTO);
+    }
+
+    @PostMapping("/commands/random")
+    public void executeRandomCommand(@RequestBody RandomCommandRequestDTO randomCommandDTO) {
+        gameService.executeRandomCommand(randomCommandDTO);
+    }
+}
